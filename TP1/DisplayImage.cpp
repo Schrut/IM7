cd6dbb6e09a85c51d1f563f7f4af21ce03dbb4e4
Schrut <gimflorian@gmail.com>
2018-01-23 16:46:51 +0100

    EX1 done

---
+++ TP1/DisplayImage.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+
+Mat image;
+Mat image_Gray;
+Mat image_Binary;
+
+int threshold_Value = 0;
+int max_Threshold_Value = 255;
+
+
+const char* trackbar_value = "Value";
+
+void image_With_Threshold (int, void*){
+  threshold( image_Gray, image_Binary, threshold_Value, max_Threshold_Value, 0);
+
+  imshow("Display Binary Image", image_Binary);
+}
+
+
+int main(int argc, char** argv )
+{
+    if ( argc != 2 )
+    {
+        printf("usage: DisplayImage.out <Image_Path>\n");
+        return -1;
+    }
+
+
+
+    image = imread( argv[1], 1 );
+
+    if ( !image.data )
+    {
+      printf("No image data \n");
+      return -1;
+    }
+
+    cvtColor(image, image_Gray, cv::COLOR_BGR2GRAY);
+
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+
+    namedWindow("Display Gray Image", WINDOW_AUTOSIZE );
+    imshow("Display Gray Image", image_Gray);
+
+    namedWindow("Display Binary Image", WINDOW_AUTOSIZE );
+
+    createTrackbar( trackbar_value, "Display Binary Image", &threshold_Value, max_Threshold_Value, image_With_Threshold );
+
+
+    while(1)
+        waitKey(0);
+
+    return 0;
+}
