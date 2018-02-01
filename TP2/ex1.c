ed20d6caa017ba291453e79f7ab0918a88540b54
Schrut <gimflorian@gmail.com>
2018-02-01 12:32:30 +0100

    Error fixed

--- TP2/ex1.c
+++ TP2/ex1.c
@@ -5,45 +5,44 @@
 #include "opencv2/imgproc/imgproc_c.h"
 #include "opencv2/imgcodecs/imgcodecs_c.h"
 
-
-
-/*Mat my_erode (Mat image_in, Mat struct_element)
+IplImage *my_erode(IplImage *image_in)
 {
-  using std::cout;
-  using std::endl;
+  int cols = image_in->width;
+  int rows = image_in->height;
+  //int size_selement = struct_element.rows;
+  int i, j;// x, y;
 
-  int rows = image_in.rows;
-  int cols = image_in.cols;
-  int size_selement = struct_element.rows;
-  int i, j, x, y;
+  IplImage* image_out;
+  image_out = image_in;
 
-  Mat image_out;
-  image_out = image_in.clone();
+    for (i=0 ; i < rows ; i++);
+        for (j=0 ; j < cols ; j++)
+            if (image_in->imageData[(i * cols + j)] != 0)
+                printf("valeur : %dl\n", image_in->imageData[(i * cols + j)]);
 
-
-
-  for (x = 2 ; x < rows ; x++)
-    for (y = 2 ; y < cols ; y++){
-
-          image_out.at<int>(x,y) = 255;
-
-    }
-}*/
+return image_out;
+}
 
 int main()
 {
 
     IplImage* image = cvLoadImage("image.jpg",0);
     IplImage* image_binary = image;
+    int* i;
 
-    //IplConvKernel* struct_element = cvCreateStructuringElementEx(3,3,-1,-1,1);
+    i = malloc (2*sizeof(int));
+    i[0]=0;
+    i[1]=1;
+
+
+    //IplConvKernel* struct_element = cvCreateStructuringElementEx(3,3,-1,-1,0,i);
 
     cvThreshold(image, image_binary, 133, 255, 0);
 
-    /*my_erode (image_binary, struct_element);*/
+    image = my_erode(image_binary);
 
     cvNamedWindow("Display Close Image", CV_WINDOW_AUTOSIZE );
-    cvShowImage("Display Close Image", image_binary);
+    cvShowImage("Display Close Image", image);
 
     while(1)
         cvWaitKey(0);
